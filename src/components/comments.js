cc67e2ad0647da214ea0da49fef973225b4823a6
joshuamoye <32655089+joshuamoye@users.noreply.github.com>
2017-11-16 20:01:37 -0500

    assignment added

--- src/components/comments.js
+++ src/components/comments.js
@@ -5,19 +5,63 @@ class Comments extends React.Component {
 constructor(){
   super();
   this.state={
-    newComments: ['working?']
+    newComments: ['working?'],
+    input: ''
   }
+  this.handleInput = this.handleInput.bind(this);
+  this.handleClick = this.handleClick.bind(this);
 }
+  handleClick(e){
+    e.preventDefault();
+    console.log(' I Just Clicked This Thing')
 
+    this.setState({
+      newComments:this.state.newComments.concat([this.state.input]),
+      input: ''
+    })
+  }
+
+  handleInput(e){
+   // console.log("look out fam")
+
+   this.setState({
+    input: e.target.value 
+   })
+   // console.log('handleInput update -' + this.state.input)
+  }
+
+  componentDidUpdate(){
+    if (this.state.input != ''){
+      // console.log('componentDidUpdate log - ' + this.state.input)
+    }
+  }
 
   render() {
-    const comments = this.props.comments.map( (item, index) => <OneComment key={index} comment={item}/>)
-    const comments = this.state.newComments.map( (item, index) => <OneComment key={index} comment={item}/>)
+    const comments = 
+      this.props.comments.map( (item, index) => <OneComment key={index} comment={item}/>)
+    const newComments = 
+      this.state.newComments.map( (item, index) => <OneComment key={index} newComment={item}/>)
+
+
 
     return (
       <div>
-        {comments}
-        {newComments}
+        
+        
+
+        <form>
+          <input placeholder= "enter comment" onChange= {this.handleInput} value = {this.state.input}/>
+          <button onClick = {this.handleClick}> Add Comment </button>
+        </form> 
+           <div className= "wrap" >
+            {newComments}
+           </div>
+        <div className= "wrap" >
+          {comments}
+        </div>
+
+       
+
       </div>
     );
   }
--- src/components/one-comment.js
+++ src/components/one-comment.js
@@ -3,10 +3,25 @@ import React from 'react';
 
 class OneComment extends React.Component{
   render(){
+
+  	if(this.props.newComment){
+  var newComments = 
+    <div className= "box box2 shadow1">
+      <h3> {this.props.newComment}</h3>
+    </div>;
+} else{
+    var comments = 
+    <div className= 'box box1 shadow1'>
+      <h3> {this.props.comment} </h3>
+    </div>
+}
+
     return (
       <div>
-        <li>{this.props.comment}</li>
+      	{newComments}
+      	{comments}
       </div>
+  
     )
   }
 }
